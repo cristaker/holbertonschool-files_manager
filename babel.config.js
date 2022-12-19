68b09aa346c59aac161de0239f4123819dfde367
cristian <cristaker1927@hotmail.com>
2022-12-19 17:48:34 -0500

    project

---
+++ babel.config.js
@@ -0,0 +1,15 @@
+lines (12 sloc)  175 Bytes
+
+
+module.exports = {
+    presets: [
+      [
+        '@babel/preset-env',
+        {
+          targets: {
+            node: 'current',
+          },
+        },
+      ],
+    ],
+};
